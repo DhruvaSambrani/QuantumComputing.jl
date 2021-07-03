de5015fd2b4ab528b4301c76a1ba98f8377b2503
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-07-03 23:53:42 +0530

    Add QuantumObjects.DensityMatrix and associated operations Fixed `inner`

--- src/QuantumObjects.jl
+++ src/QuantumObjects.jl
@@ -1,15 +1,13 @@
 module QuantumObjects
 import Base: *, ∘, kron, adjoint
-
-export QuantumObject, Ket, Bra, inner
+import LinearAlgebra: Hermitian, tr
+export QuantumObject, Ket, Bra, DensityMatrix, inner
 
 abstract type QuantumObject end
 
 struct Ket <: QuantumObject
     coefficients::AbstractArray{ComplexF64,1}
 end
-
-inner(k1::Ket, k2::Ket) = k1' * k2
 
 struct Bra <: QuantumObject
     k::Ket
@@ -23,8 +21,19 @@ Base.adjoint(b::Bra) = Ket(b)
 Base.:*(k1::Ket, k2::Ket) = inner(k1, k2)
 Base.:*(b::Bra, k::Ket) = inner(Ket(b), k)
 
+struct DensityMatrix <: QuantumObject
+    matrix::Hermitian{ComplexF64, Matrix{ComplexF64}}
+end
+
+inner(ρ::DensityMatrix, σ::DensityMatrix) = 1/2 * tr(ρ.matrix * σ.matrix) 
+inner(k1::Ket, k2::Ket) = k1.coefficients' * k2.coefficients
+
+
+Base.:*(ρ::DensityMatrix, σ::DensityMatrix) = inner(ρ, σ) 
+
 Base.kron(k1::Ket, k2::Ket) = Ket(k1.coefficients ⊗ k2.coefficients)
 Base.kron(b1::Bra, b2::Bra) = Bra(b1.k ⊗ b2.k)
+Base.kron(ρ1::DensityMatrix, ρ2::DensityMatrix) = DensityMatrix(ρ1.matrix ⊗ ρ2.matrix)
 
 ⊗ = Base.kron
 
--- src/QuantumOperations.jl
+++ src/QuantumOperations.jl
@@ -1,8 +1,8 @@
 module QuantumOperations
-import Base: *, ∘, kron
+import Base: *, ∘, kron, adjoint
 using ..QuantumObjects
 
-export QuantumOperation, Operator
+export QuantumOperation, Operator, expectation
 
 abstract type QuantumOperation end
 
@@ -10,10 +10,13 @@ struct Operator <: QuantumOperation
     trm::AbstractMatrix{ComplexF64}
 end
 
+Base.adjoint(o1::Operator) = Operator(o1.trm')
+
 Base.:∘(o1::Operator, o2::Operator) = Operator(o.trm * o2.trm)
 
 (o::Operator)(k::Ket)::Ket = Ket(o.trm * k.coefficients)
-(o::Operator)(b::Bra)::Bra = Bra(o(b.k)) 
+(o::Operator)(b::Bra)::Bra = Bra(o(b.k))
+(o::Operator)(ρ::DensityMatrix)::DensityMatrix = o.trm * ρ.matrix * (o.trm)'
 
 Base.:*(o::Operator, k::Ket) = o(k)
 Base.:*(b::Bra, o::Operator) = o(b)
